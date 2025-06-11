3ccbde70c504ca9be76a361328ca95af58074ae4
sasidharkoneru <sasidharkoneru22@gmail.com>
2025-06-11 13:17:53 +0530

    Added Cart Entity 11th june 2025

--- src/main/java/com/salesSavvy/entity/Users.java
+++ src/main/java/com/salesSavvy/entity/Users.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Users {
@@ -16,11 +17,15 @@ public class Users {
 	String gender;
 	String dob;
 	String role;
+	
+	@OneToOne
+	Cart cart;
+	
 	public Users() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Users(Long id, String username, String email, String password, String gender, String dob, String role) {
+	public Users(Long id, String username, String email, String password, String gender, String dob, String role, Cart cart) {
 		super();
 		this.id = id;
 		this.username = username;
@@ -29,6 +34,7 @@ public class Users {
 		this.gender = gender;
 		this.dob = dob;
 		this.role = role;
+		this.cart = cart;
 	}
 	public Long getId() {
 		return id;
@@ -72,9 +78,17 @@ public class Users {
 	public void setRole(String role) {
 		this.role = role;
 	}
+
+	public Cart getCart() {
+		return cart;
+	}
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
 	@Override
 	public String toString() {
 		return "Users [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
-				+ ", gender=" + gender + ", dob=" + dob + ", role=" + role + "]";
+				+ ", gender=" + gender + ", dob=" + dob + ", role=" + role + ", cart=" + cart + "]";
 	}
+	
 }
